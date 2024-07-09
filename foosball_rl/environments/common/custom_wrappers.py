588996aa9584727c898585a7fe2a7f1323e71097
Gashi <adriatik.gashi0@gmail.com>
2024-07-09 17:23:13 +0200

    Implemented HER wrapper

--- foosball_rl/environments/common/custom_wrappers.py
+++ foosball_rl/environments/common/custom_wrappers.py
@@ -1,5 +1,6 @@
+import logging
 from collections import OrderedDict
-from typing import Any, SupportsFloat, Union, Optional, Dict
+from typing import Any, Union, Optional, Dict
 
 import gymnasium as gym
 import numpy as np
@@ -9,20 +10,24 @@ from gymnasium.core import WrapperActType, ActType, WrapperObsType
 from foosball_rl.environments.constants import WHITE_GOAL_X_POSITION, FIELD_HEIGHT
 
 
-class GoalConditionWrapper(gym.Wrapper):
+class GoalConditionedWrapper(gym.Wrapper):
 
     def __init__(self, env: gym.Env):
         super().__init__(env)
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.original_observation_space = env.observation_space
         self.observation_space = self._make_observation_space()
-        self.desired_goal = np.array([WHITE_GOAL_X_POSITION, 0.0, FIELD_HEIGHT])  # The goal position of the ball
+        self.desired_goal = np.zeros(self.original_observation_space.shape[0])
+        self.desired_goal[0:3] = np.array(
+            [WHITE_GOAL_X_POSITION, 0.0, FIELD_HEIGHT])  # Where the ball should ideally be
+        # The other dimensions are irrelevant as they are not used for the reward calculation
 
     def _make_observation_space(self) -> gym.Space:
         return spaces.Dict({
-                "observation": self.original_observation_space,
-                "achieved_goal": self.original_observation_space,
-                "desired_goal": spaces.Box(low=0, high=1, shape=(3,), dtype=np.float32),
-            })
+            "observation": self.original_observation_space,
+            "achieved_goal": self.original_observation_space,
+            "desired_goal": self.original_observation_space,
+        })
 
     def _get_obs(self, obs: WrapperObsType):
         return OrderedDict(
@@ -33,23 +38,31 @@ class GoalConditionWrapper(gym.Wrapper):
             ]
         )
 
-    def step(self, action: WrapperActType) -> tuple[WrapperObsType, SupportsFloat, bool, bool, dict[str, Any]]:
+    def reset(self, *, seed: int = None, options: dict[str, Any] = None) -> tuple[WrapperObsType, dict[str, Any]]:
+        obs, info = self.env.reset(seed=seed, options=options)
+        return self._get_obs(obs), info
+
+    def step(self, action: WrapperActType):
         obs, reward, terminated, truncated, info = self.env.step(action)
         obs = self._get_obs(obs)
-        reward += float(self.compute_reward(obs["achieved_goal"], obs["desired_goal"], None).item())
+        reward += float(self.compute_reward(obs["achieved_goal"], obs["desired_goal"], info).item())
         return obs, reward, terminated, truncated, info
 
     def compute_reward(self, achieved_goal: Union[int, np.ndarray], desired_goal: Union[int, np.ndarray],
                        _info: Optional[Dict[str, Any]]) -> np.float32:
         # As we are using a vectorized version, we need to keep track of the `batch_size`
-        # if isinstance(achieved_goal, int):
-        #     batch_size = 1
-        # else:
-        #     batch_size = achieved_goal.shape[0] if len(achieved_goal.shape) > 1 else 1
+        # As we are using a vectorized version, we need to keep track of the `batch_size`
+        if isinstance(achieved_goal, int):
+            batch_size = 1
+        else:
+            batch_size = achieved_goal.shape[0] if len(achieved_goal.shape) > 1 else 1
 
-        achieved_ball_pos = achieved_goal[0:2]
+        reshaped_achieved_goal = np.array(achieved_goal).reshape(batch_size, -1)
+        reshaped_desired_goal = np.array(desired_goal).reshape(batch_size, -1)
+        achieved_ball_pos = reshaped_achieved_goal[:, 0:3]
+        desired_ball_pos = reshaped_desired_goal[:, 0:3]
 
-        distance = np.linalg.norm(achieved_ball_pos - desired_goal, axis=-1)
+        distance = np.linalg.norm(achieved_ball_pos - desired_ball_pos, axis=-1)
         return -(distance > 0).astype(np.float32)
 
 
@@ -57,21 +70,23 @@ class AddActionToObservationsWrapper(Wrapper):
 
     def __init__(self, env: gym.Env):
         super().__init__(env)
+        self.logger = logging.getLogger(self.__class__.__name__)
         self.original_observation_space = env.observation_space
         self.action_space = env.action_space
         self.action_dim = np.sum(self.action_space.shape) if env.action_space.shape != () else 1
         self.observation_space = self._extend_observation_space()
-        print("-" * 50)
-        print(f"{AddActionToObservationsWrapper.__name__}: Extended observation space from {self.original_observation_space} to {self.observation_space}")
-        print("-" * 50)
+        self.logger.info("Extended observation space from %s to %s", self.original_observation_space,
+                         self.observation_space)
 
     def _extend_observation_space(self) -> gym.Space:
         if isinstance(self.original_observation_space, gym.spaces.Box):
-            low = np.concatenate((self.original_observation_space.low, -np.inf * np.ones(self.action_dim)), dtype=self.original_observation_space.dtype)
-            high = np.concatenate((self.original_observation_space.high, np.inf * np.ones(self.action_dim)), dtype=self.original_observation_space.dtype)
+            low = np.concatenate((self.original_observation_space.low, -np.inf * np.ones(self.action_dim)),
+                                 dtype=self.original_observation_space.dtype)
+            high = np.concatenate((self.original_observation_space.high, np.inf * np.ones(self.action_dim)),
+                                  dtype=self.original_observation_space.dtype)
             return gym.spaces.Box(low=low, high=high, dtype=self.original_observation_space.dtype)
         else:
-            print(f"Observation space {self.original_observation_space} not supported for extension")
+            self.logger.error("Observation space %s not supported for extension", self.original_observation_space)
             raise NotImplementedError
 
     def reset(self, **kwargs: Any) -> Any:
