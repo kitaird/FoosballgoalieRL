bfa13051cbf5b56dd7aec6c61643a48c384d10a6
Gashi <adriatik.gashi0@gmail.com>
2024-12-02 14:03:52 +0100

    Initial commit

--- foosball_rl/environments/__init__.py
+++ foosball_rl/environments/__init__.py
@@ -1,4 +1,3 @@
-from foosball_rl.environments.foosball.single_agent import foosball_id
 from foosball_rl.environments.goalkeeper import goalkeeper_id
 
-__all__ = ['foosball_id', 'goalkeeper_id']
+__all__ = ['goalkeeper_id']
--- foosball_rl/environments/foosball/single_agent/README.md
+++
@@ -1,3 +0,0 @@
-# Foosball-v0
-... TODO: to be implemented ...
-
